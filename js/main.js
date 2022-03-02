e6e7b3484296942bc31eb2e70332554dcb0d2ee3
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-03-02 16:13:14 +0200

    [DEV]: all section without why us

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// swiper clients
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:false,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 15,
+        },
+        600: {
+            slidesPerView: 3.5,
+            spaceBetween: 20,
+        },
+        1000: {
+            slidesPerView: 6.5,
+            spaceBetween: 20,
+        },
+    },
+});
+
+// swiper team
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:false,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: '.fa-chevron-right',
+        prevEl: '.fa-chevron-left',
+
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            freeMode:false,
+        },
+        600: {
+            slidesPerView: 3.5,
+            spaceBetween: 20,
+        },
+        1000: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
